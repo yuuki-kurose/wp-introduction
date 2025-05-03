146fff14bf4ea1a7714a65b2c6aea84064842b4d
yuuki-kurose <pw1006@icloud.com>
2025-05-03 18:22:57 +0900

    Add: create header flame.
    ヘッダーの枠組み作成

--- wp-content/themes/introduction/header.php
+++ wp-content/themes/introduction/header.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/header.css">
     <?php wp_head(); ?>
 </head>
 <body>
   <header>
     <div class="header">
-      くろせ ゆうき
+      <div class="header__content">
+        <h3 class="header__title">ポートフォリオ</h3>
+        <ul class="header__tags">
+          <li><a class="header__link" href="/">about</a></li>
+          <li><a class="header__link" href="/">お問い合わせ</a></li>
+        </ul>
+      </div>
+      <img class="header__image" src="<?php echo get_template_directory_uri(); ?>/assets/image/header/header.jpg">
     </div>
   </header>
