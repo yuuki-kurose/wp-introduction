c89688e792e3601aa5ffe3c3bdfd5aa27b1b1e61
yuuki-kurose <pw1006@icloud.com>
2025-06-21 13:45:14 +0900

    Add: Create new registration and login pages and tables.
    新規登録・ログインページの作成、テーブルの作成

--- wp-content/themes/introduction/chat-register.php
+++ wp-content/themes/introduction/chat-register.php
@@ -1,26 +1,25 @@
 <?php get_header(); ?>
-<div class="login">
-  <form class="login__form">
-    <div class="login__container">
+<div class="register">
+  <form class="register__form">
+    <div class="register__container">
       <label for="username">ユーザー名</label>
-      <input type="text" id="username" placeholder="ユーザー名を入力してください" required />
+      <input type="text" id="userName" placeholder="ユーザー名を英数字8文字以内で入力してください" required />
     </div>
-    <div class="login__container">
+    <div class="register__container">
       <label for="password">パスワード</label>
-      <input type="password" id="password" placeholder="パスワードを入力してください" required />
+      <input type="password" id="passWord" placeholder="パスワードを英数字記号ありで8文字以内で入力してください" required />
     </div>
-    <div class="login__button">
-      <button type="submit" id="login-button">ログイン</button>
-      <button type="button" id="login-register"><a href="">新規登録</a></button>
+    <div class="register__button">
+      <button type="submit" id="register-button">登録する</button>
     </div>
   </form>
 </div>
 
 <script>
-  document.getElementById('login-button').addEventListener('submit', (e) => {
+  document.getElementById('register-button').addEventListener('submit', (e) => {
     e.preventDefault();
-    const userName = document.getElementById('username').value; // ユーザー名を取得
-    const passWord = document.getElementById('password').value; // パスワードを取得
+    const userName = document.getElementById('userName').value; // ユーザー名を取得
+    const passWord = document.getElementById('passWord').value; // パスワードを取得
 
     const userRegex = /^[a-z0-9_]{1,8}$/; // ユーザー名の正規表現
     const passRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{8,}$/; // パスワードの正規表現
@@ -33,7 +32,7 @@
       alert('ユーザー名を入力してください');
       return;
     } else if(passWord === '') {
-      alert('passwordを入力してください');
+      alert('パスワードを入力してください');
       return;
     };
 
@@ -46,17 +45,17 @@
       return;
     }
 
-    // ログイン情報を送信
+    // 登録情報を送信
     fetch('<?php echo admin_url("admin-ajax.php") ?>', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: new URLSearchParams({
-        action: 'login_user',
+        action: 'register_user',
         username: userName,
         password: passWord
       })
     })
-  });
+  })
 </script>
--- wp-content/themes/introduction/functions.php
+++ wp-content/themes/introduction/functions.php
@@ -12,31 +12,41 @@ function add_css_link_files() {
   if(is_home()) {
     wp_enqueue_style('home', get_stylesheet_directory_uri() . '/assets/css/home.css');
   }
-  // chatページ
-  if(get_query_var('pagename') == 'chat') {
-      wp_enqueue_style('chat', get_stylesheet_directory_uri() . '/assets/css/chat.css');
+  // 新規登録ページ
+  if(get_query_var('pagename') == 'chat-register') {
+      wp_enqueue_style('chat-register', get_stylesheet_directory_uri() . '/assets/css/chat-register.css');
   }
   // chat-signinページ
   if(get_query_var('pagename') == 'chat-signin') {
       wp_enqueue_style('chat-signin', get_stylesheet_directory_uri() . '/assets/css/chat-signin.css');
   }
+  // chatページ
+  if(get_query_var('pagename') == 'chat') {
+      wp_enqueue_style('chat', get_stylesheet_directory_uri() . '/assets/css/chat.css');
+  }
 }
 add_action('wp_enqueue_scripts', 'add_css_link_files');
 
+/**
+ * リライトルールの読み込み
+ */
 add_action('init', function() {
+  // 新規登録ページ
+  add_rewrite_rule('^chat-register/?$', 'index.php?pagename=chat-register', 'top');
+  // ログインページ
   add_rewrite_rule('^chat-signin/?$', 'index.php?pagename=chat-signin', 'top');
+  // chatページ
   add_rewrite_rule('^chat/?$', 'index.php?pagename=chat', 'top');
-});
-
-add_action('wp', function($wp_query) {
-    $slug = get_query_var('pagename');
-    error_log('現在のスラッグ: ' . $slug);
 });
 
 /**
  * カスタムテンプレート読み込み
  */
 function load_custom_template($template) {
+  // 新規登録ページ
+  if(get_query_var('pagename') == 'chat-register') {
+    return get_template_directory() . '/chat-register.php';
+  }
   // ログインページ
   if(get_query_var('pagename') == 'chat-signin') {
     return get_template_directory() . '/chat-signin.php';
