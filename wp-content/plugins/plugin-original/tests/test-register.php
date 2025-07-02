54803cf24f025c5ce50d353dec9ff07b73eea418
yuuki-kurose <pw1006@icloud.com>
2025-07-02 23:55:16 +0900

    Feat: Introducing PHPUnit.
    PHPUnitの導入

---
+++ wp-content/plugins/plugin-original/tests/test-register.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Test register
+ * description: テストDBに値が保存されるかをテストする
+ */
+
+$data = [
+  'user_name' => 'テスト',
+  'password' => 'testtest'
+];
+
+function test_to_signin() {
+  
+}
+?>
