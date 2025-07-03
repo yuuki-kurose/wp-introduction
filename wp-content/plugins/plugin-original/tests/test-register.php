aff8f695ab5d879e6f4d346814218b07079836a7
yuuki-kurose <pw1006@icloud.com>
2025-07-03 15:20:38 +0900

    Feat: Test Code Creation.
    テストコード作成
    1

--- wp-content/plugins/plugin-original/tests/test-register.php
+++ wp-content/plugins/plugin-original/tests/test-register.php
@@ -4,12 +4,33 @@
  * description: テストDBに値が保存されるかをテストする
  */
 
+// echo '現在のファイル場所' . __DIR__;
+// exit;
+// wp-load.phpを読み込むようにパスを設定
+require_once __DIR__ . '../../../../../wp-load.php';
+
 $data = [
   'user_name' => 'テスト',
   'password' => 'testtest'
 ];
 
-function test_to_signin() {
-  
+function test_to_signin($data) {
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'test_chat_users';
+  $test_result = $wpdb->insert($table_name, [
+    'username' => $data['user_name'],
+    'password' => $data['password'],
+    'created_at' => current_time('mysql')
+  ]);
+
+  if(!$test_result) {
+    echo "接続エラー：" . $wpdb->last_error . PHP_EOL;
+  } else {
+    return $test_result;
+  }
 }
+
+echo '<pre>';
+print_r(test_to_signin($data));
+echo '</pre>';
 ?>
